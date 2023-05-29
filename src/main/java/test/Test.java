ae2cb3a8be7447f55e1e6b5385ef80bb5926576d
RonanBesle <ronan.besle@gmail.com>
2023-05-29 12:03:58 +0200

    test 2

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -20,7 +20,7 @@ public class Test extends HttpServlet {
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		doGet(request, response);
 	}
 
